f3a11b566ccf9cc2b7b5445f97fbb7c49dcf11b6
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-12-10 19:14:02 +0000

    remove newline

--- lib/editor.ml
+++ lib/editor.ml
@@ -2,7 +2,6 @@
 │ SPDX-FileCopyrightText: 2025 toastal <https://toast.al/contact/>             │
 │ SPDX-License-Identifier: LGPL-2.1-or-later WITH OCaml-LGPL-linking-exception │
 └─────────────────────────────────────────────────────────────────────────────*)
-
 let find () =
 	match Sys.getenv_opt "VISUAL" with
 	| Some v -> v
