01fca4dc6a613871dd57ec98000a2c3dfd7d0bb6
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-12-10 13:00:26 +0000

    dogfood nixtamal

--- release.nix
+++ release.nix
@@ -3,12 +3,9 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later                                   │
 #──────────────────────────────────────────────────────────────────────────────┘
 let
-   pkgs_src = builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/bd88d6c13ab85cc842b93d53f68d6d40412e5a18.tar.gz";
-      sha256 = "0nxk7d12y1zq7rbfjkspprbh2wac61cjsjaidc94gpv9bd0pbsja";
-   };
+   inputs = import ./nix/tamal { };
 
-   pkgs = import pkgs_src {
+   pkgs = import inputs.nixpkgs {
       overlays = [
          (import ./nix/overlay/default.nix)
          (import ./nix/overlay/development.nix)
