2ffd302bd0f7268c5d45691170b4f87339d1b2e4
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-12-23 06:22:23 +0000

    package/nixfmt: move to dash-nix

--- nix/package/nixfmt-3-space.nix
+++ nix/package/nixfmt-3-space.nix
@@ -2,29 +2,23 @@
 # SPDX-FileCopyrightText: 2025 toastal <https://toast.al/contact/>             │
 # SPDX-License-Identifier: LGPL-2.1-or-later                                   │
 #──────────────────────────────────────────────────────────────────────────────┘
-# I don’t like *this* formatter, but it is *a* formatter. Tweaking it to expand
-# to 3 spaces, helps the maker actually read the code since 2 spaces is not
-# enough contrast (& Nix magic strings don’t support tabs).
 {
    lib,
-   writeTextFile,
-   dash,
+   writeDashBinScript,
    nixfmt-rfc-style,
-   stdenvNoCC,
 }:
 
-writeTextFile {
-   name = "nixfmt-3-space";
-   executable = true;
-   destination = "/bin/nixfmt";
+writeDashBinScript {
+   name = "nixfmt";
    text = /* sh */ ''
-      #!${lib.getExe dash}
-      exec ${lib.getExe nixfmt-rfc-style} --indent=3 "$@"
+      exec ${lib.getExe nixfmt-rfc-style} --indent="3" "$@"
    '';
-   checkPhase = ''
-      runHook preCheck
-      ${stdenvNoCC.shellDryRun} "$target"
-      runHook postCheck
-   '';
-   meta.mainProgram = "nixfmt";
+   meta = {
+      description = "nixfmt, but with 3-space indentation";
+      longDescription = ''
+         I don’t like *this* formatter, but it is *a* formatter. Tweaking it to
+         expand to 3 spaces, helps the maker actually read the code since 2
+         spaces is not enough contrast (& Nix magic strings don’t support tabs).
+      '';
+   };
 }
