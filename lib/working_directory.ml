7dec66ec52b559cc5cb80f899182b55440736834
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-12-30 07:01:39 +0000

    Working dir: use info + fix wording

--- lib/working_directory.ml
+++ lib/working_directory.ml
@@ -41,7 +41,7 @@ insert_final_newline = unset
 
 let set_up_editor_config ~dir ~content =
 	let editor_config_file = Eio.Path.(dir / ".editorconfig") in
-	Logs.app (fun m -> m "Writing new Nixtamal EditorConfig%a …" pp_native_path editor_config_file);
+	Logs.info (fun m -> m "Writing new Nixtamal EditorConfig%a …" pp_native_path editor_config_file);
 	Eio.Path.with_open_out ~create: (`Or_truncate 0o644) editor_config_file @@ fun flow ->
 	Eio.Buf_write.with_flow flow @@ fun writer ->
 	Eio.Buf_write.string writer content
@@ -50,7 +50,7 @@ let set_up_root () =
 	let dir = get () in
 	match Eio.Path.kind ~follow: true dir with
 	| `Not_found ->
-		Logs.app (fun m -> m "Creating Nixtamal directory%a" pp_native_path dir);
+		Logs.info (fun m -> m "Making Nixtamal directory%a" pp_native_path dir);
 		Eio.Path.mkdirs ~perm: 0o755 dir;
 		set_up_editor_config ~dir ~content: root_editor_config_content
 	| `Directory ->
@@ -70,7 +70,7 @@ let set_up_darcs_context_if_needed () : (unit, string) result =
 	| `Directory ->
 		Ok ()
 	| `Not_found ->
-		Logs.app (fun m -> m "Creating Nixtamal’s darcs_context directory%a" pp_native_path dir);
+		Logs.info (fun m -> m "Making Nixtamal’s darcs_context directory%a" pp_native_path dir);
 		Eio.Path.mkdirs ~perm: 0o755 dir;
 		(* as these files are just copied over, unset rules *)
 		set_up_editor_config ~dir ~content: darcs_context_editor_config_content;
