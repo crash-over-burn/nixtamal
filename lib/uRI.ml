9fe41916f5fed253052ae287b1ee29793057d50e
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2026-01-02 07:51:48 +0000

    QCheck: update naming

--- lib/uRI.ml
+++ lib/uRI.ml
@@ -13,7 +13,7 @@ let jsont : t Jsont.t =
 let gen =
 	let open QCheck.Gen in
 	let a_to_z = (char_range 'a' 'z') in
-	let* scheme = QCheck.Gen.oneofl ["http"; "https"; "ftp"; "sftp"] in
+	let* scheme = QCheck.Gen.oneof_list ["http"; "https"; "ftp"; "sftp"] in
 	let* host = string_size ~gen: a_to_z (int_bound 20) in
 	let* tld = string_size ~gen: a_to_z (int_bound 5) in
 	let* path_opt = option (string_size ~gen: a_to_z (int_bound 10)) in
